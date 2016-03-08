0df41664dce3b9694bccff03c6f4f43c75efc335
robmcarrier <robmcarrier@gmail.com>
2016-03-08 21:48:46 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(array, position=nil)
+  if array[position] == "" || array[position] == " " || array[position] == nil 
+    false
+  else
+    true
+  end
+end
